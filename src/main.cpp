d3c3e87ac00f60e70721b2b0ef24002608976466
Sozonov Andrey <andreysoz@yandex.ru>
2022-10-31 17:30:18 +0300

    feat: added lib option and install comand

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <src/config/MyPrgConfig.h>
+#include <iostream>
+#include "include/inc.h"
+#ifdef USE_SOME_LIB
+    #include <somelib_dir.h>
+#endif
+
+int main(){
+#ifdef USE_SOME_LIB
+    	std::cout << "Versionq: " << PRG_VERSION_MAJOR << std::endl;
+#endif
+#ifndef USE_SOME_LIB
+	std::cout << "asdasd" << std::endl;
+#endif
+}
