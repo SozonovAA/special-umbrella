8fc86adbe86bb00cc938ee43909cdbac20433e35
Sozonov Andrey <andreysoz@yandex.ru>
2022-11-11 22:12:39 +0300

    feat: added build script

--- configure.sh
+++ configure.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 
 cmake -S . -B out/build
-cd ./out/build
-cmake --build .
-ctest
+
--- examples/first-example/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-
-int main(int, char*[])
-{
-    auto sum =1+1;
-    std::cout << sum << std::endl;
-    return 0;
-}
